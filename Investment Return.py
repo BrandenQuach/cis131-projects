7ad568d0560c0d790c4a8b79dfe705cc34c43566
BrandenQuach <158991144+BrandenQuach@users.noreply.github.com>
2024-09-05 11:56:52 -0700

    Update and rename Investment Return to Investment Return.py

---
+++ Investment Return.py
@@ -0,0 +1,17 @@
+# Assignment: Investment Return
+# Author: Branden Quach
+# September 5, 2024
+# Calculates 7% return of $1000 after certain years
+
+def main():
+    # Variables
+    p = 1000
+    r = 0.07
+    y = [10, 20, 30]
+
+    a = {n: p * (1 + r) ** n for n in years}
+
+    for n, a in amount.items():
+        print(f"Amount after {n} years: ${a:.2f}")
+
+  main()
