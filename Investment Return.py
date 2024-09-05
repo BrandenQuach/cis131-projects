b6f4205cd19dc09723b38036644882cd05dc4f7e
BrandenQuach <158991144+BrandenQuach@users.noreply.github.com>
2024-09-05 12:02:00 -0700

    Update Investment Return.py

--- Investment Return.py
+++ Investment Return.py
@@ -5,13 +5,15 @@
 
 def main():
     # Variables
-    p = 1000
-    r = 0.07
-    y = [10, 20, 30]
+    p = 1000 # Principle Balance
+    r = 0.07 # Annual rate of return
+    y = [10, 20, 30] # Years to calculate
 
-    a = {n: p * (1 + r) ** n for n in years}
+    a = {n: p * (1 + r) ** n for n in y} # Deposit amount equation
 
-    for n, a in amount.items():
+    print(f'7% Investment Return')
+    # Repeats for all years listed
+    for n, a in a.items():
         print(f"Amount after {n} years: ${a:.2f}")
 
 main()
