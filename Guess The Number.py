803e7410144f30fb8a67ad796bc2de506882a93b
BrandenQuach <158991144+BrandenQuach@users.noreply.github.com>
2024-09-14 21:45:36 -0700

    Update Guess The Number.py

--- Guess The Number.py
+++ Guess The Number.py
@@ -1,5 +1,35 @@
 # Assignment: Miles Per Gallon
 # Author: Branden Quach
 # September 14, 2024
-# A game where you guess the number within 10 tries
+# A game where you guess the number with the fewest tries
 
+import random
+
+def main():
+    while True:
+      tries = 0
+      randomNumber = random.randint(1, 1000)
+
+      while True:
+          try:
+              guess = int(input(f'Guess my number between 1 and 1000 with the fewest guesses: '))
+              tries += 1
+
+              if guess > randomNumber:
+                  print(f'Too high. Try again.')
+              elif guess < randomNumber:
+                  print(f'Too low. Try again.')
+              else:
+                  print(f'Congratulations. You guessed the number!')
+                  break
+          except:
+              print(f'Invalid entry. Please try again.')
+              return main()
+      if tries <= 10:
+          print(f'Either you know the secret or you got lucky!')
+      else:
+          print(f'You should be able to do better!')
+      replay = input(f'Do you want to play again? (yes/no): ')
+      if replay.lower() != 'yes' or replay != 'ye' or replay != 'y':
+          break
+main()
