9569d1448633c34b2c97b4964610a4285e0fee00
BrandenQuach <158991144+BrandenQuach@users.noreply.github.com>
2024-09-28 21:16:45 -0700

    Update Telephone File.py

--- Telephone File.py
+++ Telephone File.py
@@ -8,17 +8,17 @@ import itertools
 
 # Main function
 def main():
-    # Check for errors
+    # Error validation
     try:
         # Prompt for telephone number
         number = (str(input(f'Enter a 7 digit telephone number without 0, 1, and - : ')))
-        # Input validation
+        # Input validation if number is longer than 7 digits or contains 0 or 1
         if len(number) != 7 or any(digit in number for digit in '01'):
             print(f'Input has to be 7 digits and not contain 0 or 1. Please try again.')
             return main()
         # Calls word converter function
         words = wordConverter(number)
-        # Prints all combinations
+        # Prints all word combinations into a .txt file
         with open('telephone_words.txt', mode='w') as telephone_words:
             for word in words:
                 print(word, file=telephone_words)
@@ -28,7 +28,7 @@ def main():
         return main()
 # Number to word converter
 def wordConverter(number):
-    # Dictionary of numbers to words
+    # Dictionary of numbers that correspond to words
     digitToLetters = {
         '2': 'ABC', '3': 'DEF', '4': 'GHI', '5': 'JKL', '6': 'MNO', '7': 'PQRS', '8': 'TUV', '9': 'WXYZ'
     }
@@ -37,5 +37,5 @@ def wordConverter(number):
     # Combines all combinations of numbers to letters
     combos = [''.join(word) for word in itertools.product(*letters)]
     return combos
-  
+# Calls main function
 main()
