ddc4d7dad687915d78958b4a4319a7b789f150eb
BrandenQuach <158991144+BrandenQuach@users.noreply.github.com>
2024-10-03 21:20:39 -0700

    Update Time Class.py

--- Time Class.py
+++ Time Class.py
@@ -8,6 +8,40 @@ class Time:
 
     def __init__(self, hour=0, minute=0, second=0):
         """Initialize each attribute."""
-        self.hour = hour # 0-23
-        self.minute = minute # 0-59
-        self.second = second # 0-59
+        self.total_seconds =self.to_total_seconds(hour, minute, second)
+
+    def _to_total_seconds(self, hour, minute, second):
+        return hour * 3600 + minute * 60 + second
+
+    @property
+    def hour(self):
+        return self._total_seconds // 3600
+
+    @hour.setter
+    def hour(self, value):
+        if not (0 <= value < 24):
+            raise ValueError(f'Hour must be between 0 and 23.')
+        self._total_seconds = (value * 3600) + (self.minute * 60) + self.second
+
+    @property
+    def minute(self):
+        return (self._total_seconds % 3600) // 60
+
+    @minute.setter
+    def minute(self, value):
+        if not (0 <= value < 60):
+            raise ValueError(f'Minute must be between 0 and 59.')
+        self._total_seconds = (self.hour * 3600) + (value * 60) + self.second
+
+    @property
+    def second(self):
+        return self._total_seconds % 60
+
+    @second.setter
+    def second(self, value):
+        if not (0 <= value < 60):
+            raise ValueError(f'Second must be between 0 and 59.')
+        self._total_seconds = (self.hour * 3600) + (self.minute * 60) + value
+
+    def __repr__(self):
+        return f'Time(hour={self.hour}, minute={Self.minute}, second={self.second})'
