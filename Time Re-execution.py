bdec83cf6ddac3bbfe6098a458e71918c5f2f09b
BrandenQuach <158991144+BrandenQuach@users.noreply.github.com>
2024-10-06 17:13:07 -0700

    Update Time Re-execution.py

--- Time Re-execution.py
+++ Time Re-execution.py
@@ -3,34 +3,32 @@
 # October 6, 2024
 # Class modification to store time as total seconds since midnight.
 
+# Imports original 10.4.2 timewithproperties file
 from timewithproperties import Time
-
+# Sets time
 wake_up = Time(8, 45, 50)
 print(wake_up)
-
+# Changes hour to 10
 wake_up.hour = 10
 print(wake_up)
-
+# Changes minute to 30
 wake_up.minute = 30
 print(wake_up)
-
+# Changes seconds to 30
 wake_up.second = 30
-print(wake_up)
+print(wake_up, '\n')
 
+# Imports modified 10.4.2 to include total seconds
 from timewithtotalseconds import Time
-
+# Sets time
 wake_up = Time(8, 45, 50)
 print(wake_up)
-print(wake_up.total_seconds)
-
+# Changes hour to 10
 wake_up.hour = 10
 print(wake_up)
-print(wake_up.total_seconds)
-
+# Changes minute to 30
 wake_up.minute = 30
 print(wake_up)
-print(wake_up.total_seconds)
-
+# Changes seconds to 30
 wake_up.second = 30
 print(wake_up)
-print(wake_up.total_seconds)
