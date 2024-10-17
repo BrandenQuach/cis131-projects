0c6b0f8d9d2e1fa5fae78c50f3bc15c04b59815f
BrandenQuach <158991144+BrandenQuach@users.noreply.github.com>
2024-10-16 19:38:13 -0700

    Update Abstract.py

--- Abstract.py
+++ Abstract.py
@@ -1,5 +1,76 @@
 # Lab: Software Engineering with Abstract Classes and Abstract Methods
 # Author: Branden Quach
-# October 5, 2024
-# 
+# October 16, 2024
+# Creates an Abstract Base Class for Employees
 
+from abc import ABC
+from abc import abstractmethod
+
+class Employee(ABC):
+    def __init__(self, first_name: str, last_name: str, ssn: str):
+        self._first_name = first_name
+        self._last_name = last_name
+        self._ssn = ssn
+
+    @property
+    def first_name(self):
+        return self._first_name
+
+    @property
+    def last_name(self):
+        return self._last_name
+
+    @property
+    def ssn(self):
+        return self._ssn
+
+    @abstractmethod
+    def earnings(self):
+        raise NotImplementedError
+
+    def __repr__(self):
+        return f'{self.first_name} {self.last_name}, SSN: {self.ssn}'
+
+class SalariedEmployee(Employee):
+    def __init__(self, first_name: str, last_name: str, ssn: str, weekly_salary: float):
+        super().__init__(first_name, last_name, ssn)
+        self.weekly_salary = weekly_salary
+
+    @property
+    def weekly_salary(self):
+        return self._weekly_salary
+
+    @weekly_salary.setter
+    def weekly_salary(self, value: float):
+        if value < 0:
+            raise ValueError(f'Weekly salary cannot be negative.')
+        self._weekly_salary = value
+
+    def earnings(self):
+        return self.weekly_salary
+
+    def __repr__(self):
+        return f'Salaried Employee: {super().__repr__()}, Weekly Salary: ${self.weekly_salary:.2f}'
+
+class HourlyEmployee(Employee):
+    def __init__(self, first_name: str, last_name: str, ssn: str, hours_worked: float, hourly_rate: float):
+        super().__init__(first_name, last_name, ssn)
+        self.hours_worked = hours_worked
+        self.hourly_rate = hourly_rate
+
+    @property
+    def hours_worked(self):
+        return self._hours_worked
+
+    @hours_worked.setter
+    def hours_worked(self, value: float):
+        if not (0 <= value <= 168):
+            raise ValueError(f'Hours worked must be between 0 and 168.')
+
+    def earnings(self):
+        return self.hours_worked * self.hourly_rate
+
+    def __repr__(self):
+        return(f'Hourly Employee: {super().__repr__()}, Hours Worked: {self.hours_worked}, Hourly Rate: ${self.hourly_rate:.2f}')
+
+    
