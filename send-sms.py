6a88a08e49e598f940dafd0b81f1793cc39503fa
BrandenQuach <158991144+BrandenQuach@users.noreply.github.com>
2024-11-15 22:46:33 -0700

    Update send-sms.py

--- send-sms.py
+++ send-sms.py
@@ -3,7 +3,7 @@ from sinch import SinchClient
 sinch_client = SinchClient(
     key_id="YOUR_key_id",
     key_secret="YOUR_key_secret",
-    project_id="YOUR_project_id"
+    project_id="9c4d9afd-f7b2-4f27-99da-f0ee939e3962"
 )
 
 send_batch_response = sinch_client.sms.batches.send(
