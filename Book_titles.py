88a29522f2cdbac1531893ed562c46933fdf67f5
BrandenQuach <158991144+BrandenQuach@users.noreply.github.com>
2024-11-24 21:07:36 -0700

    Update Book_titles.py

--- Book_titles.py
+++ Book_titles.py
@@ -1,6 +1,13 @@
+# Lab: Software Engineering with Abstract Classes and Abstract Methods
+# Author: Branden Quach
+# November 24, 2024
+
+# Libraries
 import sqlite3
 import pandas as pd
 
+# Connection object
 connection = sqlite3.connect('books.db')
 
+# View all titles in ascending order
 pd.read_sql("SELECT title FROM titles ORDER BY title ASC", connection)
