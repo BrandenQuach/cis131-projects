353df3fe921bfdc158522fde6981e7fead3371dd
BrandenQuach <158991144+BrandenQuach@users.noreply.github.com>
2024-11-24 21:11:13 -0700

    Update Last_name.py

--- Last_name.py
+++ Last_name.py
@@ -1,6 +1,13 @@
+# Lab: Books Database
+# Author: Branden Quach
+# November 24, 2024
+
+# Libraries
 import sqlite3
 import pandas as pd
 
+# Connection object
 connection = sqlite3.connect('books.db')
 
+# Views all authors' last names in descending order
 pd.read_sql("SELECT last FROM authors ORDER BY last DESC", connection)
