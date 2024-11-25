494b9d542ba988a5ed8185fe2851cac0813a1a1b
BrandenQuach <158991144+BrandenQuach@users.noreply.github.com>
2024-11-24 20:10:21 -0700

    Create Insert_into.py

---
+++ Insert_into.py
@@ -0,0 +1,8 @@
+import sqlite3
+
+connection = sqlite3.connect('books.db')
+
+cursor = connection.cursor()
+
+cursor = cursor.execute("""INSERT INTO authors (first, last)
+            Values('Jeff', 'Johnson')""")
