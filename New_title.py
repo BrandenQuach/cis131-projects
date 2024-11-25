26f7128f59cbe4df95d18aa644598598db893af1
BrandenQuach <158991144+BrandenQuach@users.noreply.github.com>
2024-11-24 21:06:26 -0700

    Update New_title.py

--- New_title.py
+++ New_title.py
@@ -1,13 +1,29 @@
+# Lab: Software Engineering with Abstract Classes and Abstract Methods
+# Author: Branden Quach
+# November 24, 2024
+
+# Libraries
 import sqlite3
 import pandas as pd
 
+# Connection object
 connection = sqlite3.connect('books.db')
 
-cursor = cursor.execute("""INSERT INTO authors (first, last)
-            		Values('Branden', 'Quach')""")
+# SQLite3 cursor object
+cursor = connection.cursor()
 
+# Inserts new author
+cursor = cursor.execute("""INSERT INTO authors (first, last)
+                        Values('Branden', 'Quach')""")
+
+# Inserts new ISBN to author's ID
+cursor = cursor.execute("""INSERT INTO author_ISBN (ID, ISBN)
+                        Values(6, '0123456789')""")
+
+# Inserts new title, edition, and copyright to ISBN
 cursor = cursor.execute("""INSERT INTO titles (ISBN, title, edition, copyright)
                         VALUES ('0123456789', 'Testing', 1, 2024)""")
 
+# Views all titles
 pd.read_sql('SELECT id, isbn, title, first, last FROM authors, titles',
              connection, index_col=['isbn'])
