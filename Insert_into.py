b7698b341e0d7979d1000ec1a1208789ceaf2a3b
BrandenQuach <158991144+BrandenQuach@users.noreply.github.com>
2024-11-24 20:59:51 -0700

    Update Insert_into.py

--- Insert_into.py
+++ Insert_into.py
@@ -1,8 +1,21 @@
-import sqlite3
+# Lab: Software Engineering with Abstract Classes and Abstract Methods
+# Author: Branden Quach
+# November 24, 2024
 
+# Libraries
+import sqlite3
+import pandas as pd
+
+# Connection object
 connection = sqlite3.connect('books.db')
 
+# SQLite3 cursor object
 cursor = connection.cursor()
 
+# Inserts new author
 cursor = cursor.execute("""INSERT INTO authors (first, last)
-                        Values('Jeff', 'Johnson')""")
+                        Values('Branden', 'Quach')""")
+
+# Views all authors
+pd.read_sql('SELECT id, first, last FROM authors',
+             connection, index_col=['id'])
