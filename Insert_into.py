799dbf8d9469dc9a1659c81cdca34ad92ec21c3f
BrandenQuach <158991144+BrandenQuach@users.noreply.github.com>
2024-11-24 20:15:47 -0700

    Update Insert_into.py

--- Insert_into.py
+++ Insert_into.py
@@ -5,4 +5,4 @@ connection = sqlite3.connect('books.db')
 cursor = connection.cursor()
 
 cursor = cursor.execute("""INSERT INTO authors (first, last)
-            Values('Jeff', 'Johnson')""")
+                        Values('Jeff', 'Johnson')""")
